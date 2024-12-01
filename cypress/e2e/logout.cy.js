b307eb76a0afc5e2da5b18ccb6e701d0161c18c0
Niculina Pop <niculinapop14@gmail.com>
2024-12-01 10:43:19 +0200

    Use locators from locatorPath and login function in logout

--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -1,14 +1,14 @@
-describe('Logout test-saucedemo', () => {
-  it('should display back the login page', () => {
-    cy.visit('https://www.saucedemo.com/', {timeout : 30000});
-    cy.get('#user-name').type('standard_user');
-    cy.get('#password').type('secret_sauce');
-    cy.get('#login-button', { timeout: 10000 }).should('be.visible').click();
-   // cy.get('#login-button').click();
-    cy.get('#react-burger-menu-btn').click()
-    cy.get('#logout_sidebar_link').click();
+const {WEB} = require('../locators/locatorPath');
+import Login from '../helpers/login';
 
-    cy.get('#login-button', { timeout: 10000 }).should('be.visible');
+describe('Logout test-saucedemo', () => {
+  it('should display back the login page', async () => {
+    await Login.login();
+   
+    cy.get(WEB.MENU.MENU_BUTTON).click()
+    cy.get(WEB.MENU.LOGOUT_BUTTON).click();
+
+    cy.get(WEB.LOGIN.LOGIN_BUTTON, { timeout: 10000 }).should('be.visible');
     
   })
 })
--- cypress/locators/locatorPath.js
+++ cypress/locators/locatorPath.js
@@ -11,6 +11,10 @@ const WEB = {
   },
   MESSAGE: {
     ERROR_MESSAGE: '.error-message-container.error'
+  },
+  MENU: {
+    MENU_BUTTON: '#react-burger-menu-btn',
+    LOGOUT_BUTTON: '#logout_sidebar_link'
   }
 };
 
