e001da98796d88d1f197b9711bec7f0690cab28f
Niculina Pop <niculinapop14@gmail.com>
2024-12-01 10:54:33 +0200

    Use locators from locatorPath and login function in back_to_products_button

--- cypress/e2e/back_to_products_button.cy.js
+++ cypress/e2e/back_to_products_button.cy.js
@@ -1,13 +1,13 @@
-describe('Back to products button functionality', () => {
-  it('should navigate back to homepage', () => {
-    cy.visit('https://www.saucedemo.com/', {timeout : 30000});
-    cy.get('#user-name').type('standard_user');
-    cy.get('#password').type('secret_sauce');
-    cy.get('#login-button', { timeout: 10000 }).should('be.visible').click();
-    cy.xpath('//*[@id="item_4_title_link"]/div').click();
-    cy.get('#back-to-products').click();
+const {WEB} = require('../locators/locatorPath');
+import Login from '../helpers/login';
 
-    cy.xpath('//*[@id="header_container"]/div[2]/div/span/select').should('be.visible');
+describe('Back to products button functionality', () => {
+  it('should navigate back to homepage',async () => {
+    await Login.login();
+    cy.xpath(WEB.PRODUCT.PRODUCT_LINK).click();
+    cy.get(WEB.PRODUCT.BACK_TO_PRODUCTS_BUTTON).click();
+
+    cy.get(WEB.PRODUCT.BACK_TO_PRODUCTS_BUTTON).should('not.exist');
 
   })
 })
