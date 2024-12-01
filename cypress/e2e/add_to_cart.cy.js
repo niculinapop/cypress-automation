5654f919fbd16985417c8f1a486908a2dd2a67d1
Niculina Pop <niculinapop14@gmail.com>
2024-12-01 11:05:56 +0200

    Add new locator from locatorPath in add_to_cart

--- cypress/e2e/add_to_cart.cy.js
+++ cypress/e2e/add_to_cart.cy.js
@@ -7,7 +7,7 @@ describe('Adding a product to cart', () => {
     cy.get(WEB.CART.ADD_TO_CART_BUTTON).click();
     cy.xpath(WEB.CART.CART_LINK).click();
 
-    cy.xpath('//*[@id="item_4_title_link"]/div').should('be.visible');
+    cy.xpath(WEB.PRODUCT.PRODUCT_LINK).should('be.visible');
     
   })
 })
