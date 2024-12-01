1d8ff9e60810b220c06957870da417c291594c40
Niculina Pop <niculinapop14@gmail.com>
2024-12-01 10:33:49 +0200

    Use locators from locatorPath  in invalid_login

--- cypress/e2e/invalid_login.cy.js
+++ cypress/e2e/invalid_login.cy.js
@@ -1,17 +1,13 @@
+const {WEB} = require('../locators/locatorPath');
+
 describe('Invalid login test-soucedemo', () => {
   it('should display an error for invalid credentials', () => {
     cy.visit('https://www.saucedemo.com/');
-    cy.get('#user-name').type('error_user');
-    cy.get('#password').type('secret_sauce');
-    cy.get('#login-button').click();
-    cy.get('body').then(($body) => {
-      if ($body.find('[data-test="error"]').length) {
-        cy.get('[data-test="error"]').should('be.visible');
-      } else {
-        cy.log('Error message not found in DOM');
-      }
-    }); 
+    cy.get(WEB.LOGIN.USERNAME_INPUT).type('me_user');
+    cy.get(WEB.LOGIN.PASSWORD_INPUT).type('secret_sauce');
+    cy.get(WEB.LOGIN.LOGIN_BUTTON).click();
+   
+    cy.get(WEB.MESSAGE.ERROR_MESSAGE).should('be.visible');
   
-
   });
 });
--- cypress/locators/locatorPath.js
+++ cypress/locators/locatorPath.js
@@ -8,6 +8,9 @@ const WEB = {
   CART: {
     ADD_TO_CART_BUTTON: "#add-to-cart-sauce-labs-backpack",
     CART_LINK: '//*[@id="shopping_cart_container"]/a'
+  },
+  MESSAGE: {
+    ERROR_MESSAGE: '.error-message-container.error'
   }
 };
 
