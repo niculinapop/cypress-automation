fed76dd0789e45e5f769abbd0d1cd2bb43e02d26
Niculina Pop <niculinapop14@gmail.com>
2024-12-01 10:50:34 +0200

    Use locators from locatorPath and login function in product_detail_page

--- cypress/e2e/product_detail_page.cy.js
+++ cypress/e2e/product_detail_page.cy.js
@@ -1,11 +1,11 @@
+const {WEB} = require('../locators/locatorPath');
+import Login from '../helpers/login';
+
 describe('Verify access after clicking on a product', () => {
-  it('should navigate to the product detail page when clicking on a product', () => {
-    cy.visit('https://www.saucedemo.com/', {timeout : 30000});
-    cy.get('#user-name').type('standard_user');
-    cy.get('#password').type('secret_sauce');
-    cy.get('#login-button', { timeout: 10000 }).should('be.visible').click();
-    cy.xpath('//*[@id="item_4_title_link"]/div').click();
+  it('should navigate to the product detail page when clicking on a product',async () => {
+    await Login.login();
+    cy.xpath(WEB.PRODUCT.PRODUCT_LINK).click();
     
-    cy.get('#back-to-products').should('be.visible');
+    cy.get(WEB.PRODUCT.BACK_TO_PRODUCTS_BUTTON).should('be.visible');
   })
 })
--- cypress/locators/locatorPath.js
+++ cypress/locators/locatorPath.js
@@ -3,7 +3,6 @@ const WEB = {
     USERNAME_INPUT: '#user-name',
     PASSWORD_INPUT: '#password',
     LOGIN_BUTTON: '#login-button'
-
   },
   CART: {
     ADD_TO_CART_BUTTON: "#add-to-cart-sauce-labs-backpack",
@@ -15,6 +14,10 @@ const WEB = {
   MENU: {
     MENU_BUTTON: '#react-burger-menu-btn',
     LOGOUT_BUTTON: '#logout_sidebar_link'
+  },
+  PRODUCT: {
+    PRODUCT_LINK: '//*[@id="item_4_title_link"]/div',
+    BACK_TO_PRODUCTS_BUTTON: '#back-to-products'
   }
 };
 
